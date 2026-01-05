2fd76f657be68677fb54efc62e196bafb1e46468
RoAr29 <agrawalsp_2@rknec.edu>
2026-01-05 22:57:45 +0530

    Time: 9 ms (8.12%) | Memory: 48 MB (8.80%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+
+        for(int i=0; i<nums.length; i++){
+            if(i!=nums[i]){
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
